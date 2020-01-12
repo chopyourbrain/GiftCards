acd73ccbdc8302d51c79cd3ac1d06bab669f44a6
Mikhail Kuznetsov <qtd130@gmail.com>
2020-01-12 14:56:47 +0300

    Delete useless scope

--- app/src/main/java/com/example/giftcards/di/scope/ActivityScope.kt
+++
@@ -1,8 +0,0 @@
-package com.example.giftcards.di.scope
-
-import javax.inject.Scope
-
-@Scope
-@kotlin.annotation.MustBeDocumented
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class ActivityScope
